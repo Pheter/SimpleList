08c588b366d985240cdf1c99d845d6d01d9c7772
Peter Horne <peterhorne92@gmail.com>
2010-11-15 23:07:38 +0000

    Modified the Index Resource to make use of the new syntax for generating Twig views

--- resources/Index.php
+++ resources/Index.php
@@ -4,12 +4,12 @@ namespace Seed\Resources;
 
 class Index {
     
-    private $Twig;
+    private $view;
     private $Cake;
     
     public function __construct($plugins) {
         
-        $this->Twig = $plugins->Twig;
+        $this->view = $plugins->Twig;
         $this->Cake = new \Seed\Models\Cake;
     }
     
@@ -23,8 +23,9 @@ class Index {
         $this->Cake->eatSlice();
         $this->Cake->eatSlice();
         
-        $template = $this->Twig->loadTemplate('index.html');
-        $template->display(array('start' => $startingNumOfSlices,
-                                 'current' => $this->Cake->numOfSlices()));
+        return $this->view->render('index.html', array(
+                                'start' => $startingNumOfSlices,
+                                'current' => $this->Cake->numOfSlices(),
+                            ));
     }
 }
